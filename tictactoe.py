846e4154a04245dc1a6717b6bc462415f88c770d
fargofremling <alicia.fremling@gmail.com>
2016-06-08 16:46:11 -0400

    add player1_move function

--- tictactoe.py
+++ tictactoe.py
@@ -1,43 +1,51 @@
 
 game_moves = [' ',' ',' ', ' ',' ',' ', ' ',' ',' ']
-player1_input = ' '
 
-#def player1_moves():
+#player1_input = ''
 
-#	player1_input = raw_input("Player 1, where would you like to place your first X? ").lower
+def player1_moves(player1_input):
 	
-	#for player1_input:
-# 	if player1_input == "tl":
-# 		game_moves[0] = 'X'
-# 	
-# 	elif player1_input == "tm":
-# 		game_moves[1] = 'X'
-# 
-# 	elif player1_input == "tr":
-# 		game_moves[2] = 'X'
-# 	
-# 	elif player1_input == "ml":
-# 		game_moves[3] = 'X'
-# 	
-# 	elif player1_input == "mm":
-# 		game_moves[4] = 'X'
-# 	
-# 	elif player1_input == "mr":
-# 		game_moves[5] = 'X'
-# 	
-# 	elif player1_input == "bl":
-# 		game_moves[6] = 'X'
-# 	
-# 	elif player1_input == "bm":
-# 		game_moves[7] = 'X'
-# 	
-# 	elif player1_input == "br":
-# 		game_moves[8] = 'X'
-# 
-# 	else:
-# 		player1_move1 = raw_input("That is not a valid entry. Please try a different locations. ").lower()
-# 		
-# 	gameboard()
+	while True:
+		if player1_input == "tl":
+			game_moves[0] = 'X'
+			break
+	
+		elif player1_input == "tm":
+			game_moves[1] = 'X'
+			break
+
+		elif player1_input == "tr":
+			game_moves[2] = 'X'
+			break
+	
+		elif player1_input == "ml":
+			game_moves[3] = 'X'
+			break
+	
+		elif player1_input == "mm":
+			game_moves[4] = 'X'
+			break
+	
+		elif player1_input == "mr":
+			game_moves[5] = 'X'
+			break
+	
+		elif player1_input == "bl":
+			game_moves[6] = 'X'
+			break
+	
+		elif player1_input == "bm":
+			game_moves[7] = 'X'
+			break
+	
+		elif player1_input == "br":
+			game_moves[8] = 'X'
+			break
+			
+		else:
+			player1_input = raw_input("That is not a valid entry. Please try a different locations.").lower() 
+			continue
+	gameboard()
 	
 def gameboard():
 	
@@ -45,7 +53,7 @@ def gameboard():
 	print "---------"
 	print game_moves[3],"|",game_moves[4], "|", game_moves[5]
 	print "---------"
-	print game_moves[6],"|",game_moves[6], "|", game_moves[8]
+	print game_moves[6],"|",game_moves[7], "|", game_moves[8]
 	print "\n"
 
 def tictactoe():
@@ -69,56 +77,11 @@ def tictactoe():
 	print "BL|BM|BR"
 	print "\n"
 	gameboard()
-	
-	#player1_move1()
-	
-	player1_move1 = raw_input("Player 1, where would you like to place your first X? ").lower()
-	#if player1_move1 in ["tl", "tm", "tr", "ml", "c", "mr", "bl", "bm", "br"]:
-	#player1_move1 = player1_input
-	#player1_moves()
-	
-	while True:
-		if player1_move1 == "tl":
-			game_moves[0] = 'X'
-			break
-	
-		elif player1_move1 == "tm":
-			game_moves[1] = 'X'
-			break
 
-		elif player1_move1 == "tr":
-			game_moves[2] = 'X'
-			break
 	
-		elif player1_move1 == "ml":
-			game_moves[3] = 'X'
-			break
+	player1_move1 = raw_input("Player 1, where would you like to place your first X? ")
+	player1_moves(player1_input = player1_move1.lower())
 	
-		elif player1_move1 == "mm":
-			game_moves[4] = 'X'
-			break
-	
-		elif player1_move1 == "mr":
-			game_moves[5] = 'X'
-			break
-	
-		elif player1_move1 == "bl":
-			game_moves[6] = 'X'
-			break
-	
-		elif player1_move1 == "bm":
-			game_moves[7] = 'X'
-			break
-	
-		elif player1_move1 == "br":
-			game_moves[8] = 'X'
-			break
-			
-		else:
-			player1_move1 = raw_input("That is not a valid entry. Please try a different locations.").lower() 
-			continue
-		
-	gameboard()
 	
 	player2_move1 = raw_input("Player 2, where would you like to place your first O? ").lower()
 	while True:	
@@ -169,50 +132,153 @@ def tictactoe():
 	gameboard()
 	
 	player1_move2 = raw_input("Player 1, where would you like to place your second X? ").lower()
+	player1_moves(player1_input = player1_move2.lower())
+	
+
+	
+	player2_move2 = raw_input("Player 2, where would you like to place your second 0? ").lower()
 	
 	while True:
-		if player1_move2 == player1_move1 or player1_move2 == player2_move1:
-			player1_move2 = raw_input("That space has already been taken. Please select another space. ").lower() 		
+		if player2_move2 == player1_move1 or player1_move2 == player2_move1 or player2_move2 == player1_move2:
+			player2_move2 = raw_input("That space has already been taken. Please select another space. ").lower() 		
 			continue
 		
-		elif player1_move2 == "tl":
+		elif player2_move2 == "tl":
+			game_moves[0] = 'O'
+			break
+
+		elif player2_move2 == "tm":
+			game_moves[1] = 'O'
+			break
+
+		elif player2_move2 == "tr":
+			game_moves[2] = 'O'
+			break
+
+		elif player2_move2 == "ml":
+			game_moves[3] = 'O'
+			break
+
+		elif player2_move2 == "mm":
+			game_moves[4] = 'O'
+			break
+
+		elif player2_move2 == "mr":
+			game_moves[5] = 'O'
+			break
+
+		elif player2_move2 == "bl":
+			game_moves[6] = 'O'
+			break
+
+		elif player2_move2 == "bm":
+			game_moves[7] = 'O'
+			break
+
+		elif player2_move2 == "br":
+			game_moves[8] = 'O'
+			break
+		
+		else:
+			player2_move2 = raw_input("That is not a valid entry. Please try a different locations.").lower() 
+			continue
+		
+	gameboard()
+	
+	player1_move3 = raw_input("Player 1, where would you like to place your third X? ").lower()
+	
+	while True:
+		if player1_move3 == player1_move1 or player1_move3 == player2_move1 or player1_move3 == player1_move2 or player1_move3 == player2_move2:
+			player1_move3 = raw_input("That space has already been taken. Please select another space. ").lower() 		
+			continue
+		
+		elif player1_move3 == "tl":
 			game_moves[0] = 'X'
 			break
 
-		elif player1_move2 == "tm":
+		elif player1_move3 == "tm":
 			game_moves[1] = 'X'
 			break
 
-		elif player1_move2 == "tr":
+		elif player1_move3 == "tr":
 			game_moves[2] = 'X'
 			break
 
-		elif player1_move2 == "ml":
+		elif player1_move3 == "ml":
 			game_moves[3] = 'X'
 			break
 
-		elif player1_move2 == "mm":
+		elif player1_move3 == "mm":
 			game_moves[4] = 'X'
 			break
 
-		elif player1_move2 == "mr":
+		elif player1_move3 == "mr":
 			game_moves[5] = 'X'
 			break
 
-		elif player1_move2 == "bl":
+		elif player1_move3 == "bl":
 			game_moves[6] = 'X'
 			break
 
-		elif player1_move2 == "bm":
+		elif player1_move3 == "bm":
 			game_moves[7] = 'X'
 			break
 
-		elif player1_move2 == "br":
+		elif player1_move3 == "br":
 			game_moves[8] = 'X'
 			break
 		
 		else:
-			player1_move2 = raw_input("That is not a valid entry. Please try a different locations.").lower() 
+			player1_move3 = raw_input("That is not a valid entry. Please try a different locations.").lower() 
+			continue
+		
+	gameboard()
+	
+	player2_move3 = raw_input("Player 1, where would you like to place your third O? ").lower()
+	
+	while True:
+		if player2_move3 == player1_move1 or player2_move3 == player2_move1 or player2_move3 == player1_move2 or player2_move3 == player2_move2 or player2_move3 == player1_move3:
+			player1_move3 = raw_input("That space has already been taken. Please select another space. ").lower() 		
+			continue
+		
+		elif player2_move3 == "tl":
+			game_moves[0] = 'O'
+			break
+
+		elif player2_move3 == "tm":
+			game_moves[1] = 'O'
+			break
+
+		elif player2_move3 == "tr":
+			game_moves[2] = 'O'
+			break
+
+		elif player2_move3 == "ml":
+			game_moves[3] = 'O'
+			break
+
+		elif player2_move3 == "mm":
+			game_moves[4] = 'O'
+			break
+
+		elif player2_move3 == "mr":
+			game_moves[5] = 'O'
+			break
+
+		elif player2_move3 == "bl":
+			game_moves[6] = 'O'
+			break
+
+		elif player2_move3 == "bm":
+			game_moves[7] = 'O'
+			break
+
+		elif player2_move3 == "br":
+			game_moves[8] = 'O'
+			break
+		
+		else:
+			player2_move3 = raw_input("That is not a valid entry. Please try a different locations.").lower() 
 			continue
 		
 	gameboard()
