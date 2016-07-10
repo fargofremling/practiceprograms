9a2298efdaffb6dedc7548899b7dd0bee6268d0c
fargofremling <alicia.fremling@gmail.com>
2016-07-09 20:22:13 -0700

    Starting to build out weight unit input function

--- coin_estimator.py
+++ coin_estimator.py
@@ -13,30 +13,38 @@
 # Dime = 2.268g, 50 per roll, $5.00, total weight per roll: 112.4g
 # Quarter = 5.67g, 40 per roll, $10.00, total weight per roll: 226.8g
 
-# 100g = 0.22lbs
+# 1oz = 28.35g
 import math
 
 coin_weights = {'pennies': ' ', 'nickels': ' ', 'dimes': ' ', 'quarters': ' '}
     
 def get_unit():
 
-    print "Would you like to use grams or pounds as your weight unit?"
+    print "Would you like to use grams or ounces as your weight unit?"
     weight_type = raw_input("> ").lower
-#     if weight_type in [gram, grams, g]:
-#         return grams
-#     elif weight_type in [pound, pounds, lb, lbs]:
-#         return pounds
+    if weight_type in ["gram", "grams", "gr", "g"]:
+        return grams
+    elif weight_type in ["ounce", "ounces", "oz.", "oz"]:
+        return ounces
+#     else:
+#         Display error message when user puts in an entry not in the above lists. 
 
 def get_weights():
 
+    user_input_weight_type = get_unit()
+
+    print user_input_weight_type
     print  "Please enter the total weight for each of the following coins."
+    
     coin_weights['pennies'] = raw_input("\nPennies:\n> ")
     coin_weights['nickels'] = raw_input("\nNickels:\n> ")
     coin_weights['dimes'] = raw_input("\nDimes:\n> ")
     coin_weights['quarters'] = raw_input("\nQuarters:\n> ")
     
 def number_wrappers():
-   
+
+    get_weights()
+
     penny_wrappers = math.ceil(int(coin_weights['pennies']) / 125)
     print "You will need,", penny_wrappers, "penny wrappers.\n"
     
@@ -50,8 +58,8 @@ def number_wrappers():
     print "You will need,", quarter_wrappers, "quarter wrappers.\n"
 
 def main():
-    get_unit()
-    get_weights()
+
+    
     number_wrappers()
     
 main()
