bec0cd366d92deee235d4196bee331fdbace219c
fargofremling <alicia.fremling@gmail.com>
2016-07-21 22:35:36 -0700

    Add Rectangles practice program.

---
+++ rectangles.py
@@ -0,0 +1,11 @@
+# Challenge #276 [Easy] Rectangles
+# Given a word, a width and a length, you must print a rectangle with the word
+# of the given dimensions.
+
+# The input is a string word, a width and a height
+
+def get_input():
+    string = raw_input("What word would you like to put into a rectangel? \n> ")
+    
+    
+get_input()
