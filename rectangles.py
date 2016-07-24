211b896ac60588a710211b18d832ffa9badb0bb7
fargofremling <alicia.fremling@gmail.com>
2016-07-23 23:11:42 -0700

    String splitting and basic rectangle printing

--- rectangles.py
+++ rectangles.py
@@ -1,15 +1,18 @@
 # Challenge #276 [Easy] Rectangles
+# https://www.reddit.com/r/dailyprogrammer/comments/4tetif/20160718_challenge_276_easy_recktangles/
 # Given a word, a width and a length, you must print a rectangle with the word
 # of the given dimensions.
 
 # The input is a string word, a width and a height
 
 def get_input():
-    string = raw_input("What word would you like to put into a rectangel? \n> ")
-    print len(string)
+    string = raw_input("What word would you like to put into a rectangel? \n> ").upper()
     
+    split = list(string)
     
-    print index(string, 0, len(string))
-    
-    
+    print split[0], " ", split[1], " ", split[2], " ", split[3], " "
+    print split[1], "         ", split[2]
+    print split[2], "         ",split[1]
+    print split[3], " ", split[2], " ", split[1], " ", split[0], " "
+
 get_input()
