8cf9ac21f5d6304bddf53febf8838ac921ff31e5
fargofremling <alicia.fremling@gmail.com>
2016-08-01 20:54:40 -0700

    Correct sum formula resulting in the correct answer.

--- fibonacci_even_sum.py
+++ fibonacci_even_sum.py
@@ -1,41 +1,45 @@
-# By considering the terms in the Fibonacci sequence whose values do not exceed 
-# four million, find the sum of the even-valued terms.
+print ("By considering the terms in the Fibonacci sequence whose values do not exceed " 
+"four million, find the sum of the even-valued terms.")
 
 evens = []
 
 def the_sequence():
-    
+
+    term = 3
+
     # establishes first term in the sequence
     f = 0
-    
+
     # establishes second term in the sequence
     g = 1
-    
-    while f < 40:
-    
-        f = f + g
-        print f
-        
-        if f % 2 == 0:
-            evens.append(f)
-            print evens
-            f += 1
+
+    while f < 4000000 or g < 4000000:
+
+        if term % 2 == 0:
+            f = f + g
+            term += 1
+
+            if f % 2 == 0:
+                evens.append(f)
+
+            else: 
+                continue
+
         else:
-            continue
-       #  else:
-#             g = g + f
-#             if g % 2 == 0:
-#                 evens.append(g)
-#             else:
-#                 continue
+            g = g + f
+            term += 1
+
+            if g % 2 == 0:
+                evens.append(g)
+
+            else: 
+                continue
 
 the_sequence()
-
-print evens
 
 def add(x, y):
     return x + y
 
 sum = reduce(add, evens)
 
-print sum
+print "Sum =", sum
