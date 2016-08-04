b62818d41f60cbee10380583b78415784ae989c5
fargofremling <alicia.fremling@gmail.com>
2016-08-03 23:29:33 -0700

    This is my test commit

--- prime_factor.py
+++ prime_factor.py
@@ -2,28 +2,22 @@
 # What is the largest prime factor of the number 600851475143 ?
 
 # Python program to check if the input number is prime or not
-
-# take input from the user
 num = 13195
 
-primes = []
+primes = [2]
 
 # prime numbers are greater than 1
-for x in xrange(num):
-    if x % 2 != 0:
-        if (num % x) != 0:
-            primes.append(x)
-            x += 1
+for y in xrange(num):
+    if y % 2 != 0:
+        if (num % y) == 0:
+            if (y % x for x in primes) != 0:
+                primes.append(x)
+            else:
+                continue
         else:
-            x += 1
-       # check for factors
-       # for i in range(2,num):
-#            if (num % i) == 0:
-#                print num,"is not a prime number"
-#                print i,"times",num//i,"is",num
-#                break
+            continue
     else:
-       x += 1
+        continue
 
 print primes
 
