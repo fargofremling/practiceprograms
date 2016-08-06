86d29f98f7d8a58a3301109141804601479ffd75
fargofremling <alicia.fremling@gmail.com>
2016-08-05 23:34:52 -0700

    Euler Project Problem 4 start

--- palindrome_number.py
+++ palindrome_number.py
@@ -2,3 +2,35 @@
 # product of two 2-digit numbers is 9009 = 91 × 99.
 # 
 # Find the largest palindrome made from the product of two 3-digit numbers.
+
+
+def palindrome():
+
+   while True:
+        user_input = raw_input("What word would you like to test? ")
+
+        palindrome_test = user_input.lower()
+
+        reverse = palindrome_test[::-1]
+
+        if palindrome_test == reverse:
+            isPalindrome = True
+
+        else:
+            isPalindrome = False
+
+        if isPalindrome:
+            print "Well, would you look at that;", user_input, "is a palindrome! \n"
+        else:
+            print "Sorry, Charlie,", user_input, "is not a palindrome. \n"
+
+        play_again = raw_input("Would you like to test another word? Y or N \n")
+    
+        if play_again in ["y", "Y", "yes", "YES"]:
+            continue
+        
+        else:
+            print "Ciao!"
+            break
+
+palindrome()
