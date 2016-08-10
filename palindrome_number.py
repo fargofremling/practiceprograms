19620dad66394d5f8f50b3c632b404ce6961399c
fargofremling <alicia.fremling@gmail.com>
2016-08-09 22:37:00 -0700

    Different approach, still not right.

--- palindrome_number.py
+++ palindrome_number.py
@@ -7,28 +7,46 @@
 
 def palindrome():
 
-    for y in range(999, 99, -1):
+    a = True
     
-        for x in xrange(999, 9, -1):
+    product_number = 0
+    
+    y = 999
+
+    x = 999
+
+    while a is True:
+        
+           
+        product = x * y
             
-            product = x * y
+        product_string = str(product) 
             
-            product_string = str(product) 
+        reverse = product_string[::-1]
+        print y, x, product
+    
+        if product_string == reverse:
+                
+            print y
             
-            reverse = product_string[::-1]
-         
-            if product_string == reverse:
-                
-                print y
-                
-                print x
-                
-                print product_string
-                
-                break
+            print x
             
-            else:
-                continue
-        break
+            print product_string
+                
+            return a is False
+            
+        elif product_number % 2 != 0:
+            y -= 1
+            print y
+            product_number += 1
+            print product_number
+            # return a is True
+            
+        else:
+            x -= 1
+            print x
+            product_number += 1
+            print product_number
+            # return a is True
 
 palindrome()
