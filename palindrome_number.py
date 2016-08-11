cf97ac0a07e79a426be4f2722dae538313451e5d
fargofremling <alicia.fremling@gmail.com>
2016-08-10 23:28:24 -0700

    Still experimenting with different approaches

--- palindrome_number.py
+++ palindrome_number.py
@@ -11,11 +11,11 @@ def palindrome():
     
     product_number = 0
     
-    y = 999
+    y = 997
 
     x = 999
 
-    while a is True:
+    while True:
         
            
         product = x * y
@@ -23,30 +23,23 @@ def palindrome():
         product_string = str(product) 
             
         reverse = product_string[::-1]
+        
         print y, x, product
     
         if product_string == reverse:
-                
-            print y
-            
-            print x
             
             print product_string
                 
-            return a is False
+            return False
             
         elif product_number % 2 != 0:
-            y -= 1
-            print y
+            y -= 2
+            x += 1
             product_number += 1
-            print product_number
-            # return a is True
             
         else:
-            x -= 1
-            print x
+            x -= 2
+            y += 1
             product_number += 1
-            print product_number
-            # return a is True
 
 palindrome()
