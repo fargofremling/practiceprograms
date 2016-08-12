f59c29ca33a2b15e9ce769dd978a66cc6b690824
fargofremling <alicia.fremling@gmail.com>
2016-08-11 22:13:39 -0700

    Start of PE problem #5

---
+++ smallest_multiple.py
@@ -0,0 +1,31 @@
+# 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 
+# without any remainder.
+# 
+# What is the smallest positive number that is evenly divisible by all of the numbers
+#  from 1 to 20?
+
+#20 (includes 2, 4, 5, 10)
+#19
+#18 (includes 2, 3, 6, 9)
+#17
+#16 (includes 2, 8
+#15 (includes 3, 5)
+#14 (includes 2, 7)
+#13
+#12 (includes 2, 3, 4, 6)
+#11
+# 
+# Prime Numbers
+# 1, 2, 3, 5, 7, 11, 13, 17, 19
+# 
+# prime_product = 2*3*5*7*11*13*17*19
+# 
+# print prime_product
+
+# prime_product is not correct
+
+# largest_factors = 11*12*13*14*15*16*17*18*19*20
+
+# print largest_factors
+
+# largest_factors is not correct
