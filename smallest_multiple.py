540f4dbaca352e82ecdd43fb232dce4765d92e8a
fargofremling <alicia.fremling@gmail.com>
2016-08-12 23:50:04 -0700

    Some experimenting

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -29,3 +29,40 @@
 # print largest_factors
 
 # largest_factors is not correct
+
+numbers = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+print numbers 
+
+def multiple():
+
+    for y in xrange(6000000, 10000000):
+            
+            if y % 20 != 0:
+                print "1 nope"
+            else:
+                if y % 19 != 0:
+                    print "2 nope"
+                else:
+                    if y % 18 != 0:
+                        print "3 nope"
+                    else:
+                        if y % 17 != 0:
+                            print "4 nope"
+                        else:
+                            if y % 16 != 0:
+                                print "5 nope"
+                            else:
+                                if y % 15 != 0:
+                                    print "6 nope"
+                                else:
+                                    if y % 14 != 0:
+                                        print "7 nope"
+                                    else:
+                                        if y % 13 != 0:
+                                            print "8 nope"
+                                        else:
+                                            print y
+                                            return
+
+multiple()
