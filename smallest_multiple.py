6d9eeee16756d96b994f848386be19a08e50e1c9
fargofremling <alicia.fremling@gmail.com>
2016-08-13 23:56:23 -0700

    Correct answer, need to clean up code

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -4,17 +4,31 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers
 #  from 1 to 20?
 
-#20 (includes 2, 4, 5, 10)
+#20 (2*2*5)
 #19
-#18 (includes 2, 3, 6, 9)
+#18 (2*3*3)
 #17
-#16 (includes 2, 8
-#15 (includes 3, 5)
-#14 (includes 2, 7)
+#16 (2*2*2*2)
+#15 (3*5)
+#14 (2*7)
 #13
-#12 (includes 2, 3, 4, 6)
+#12 (2*2*3)
 #11
-# 
+#  CONTINUE
+#9 (3*3)
+#8 (2*2*2)
+#7
+#6 (2*3)
+#5
+#4 (2*2)
+#3
+#2
+
+# 1, 2, 3, 5, 7, 11, 13, 17, 19
+
+number = 2*2*2*2*3*3*5*7*11*13*17*19
+
+print number
 # Prime Numbers
 # 1, 2, 3, 5, 7, 11, 13, 17, 19
 # 
@@ -30,39 +44,39 @@
 
 # largest_factors is not correct
 
-numbers = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
-print numbers 
-
-def multiple():
-
-    for y in xrange(6000000, 10000000):
-            
-            if y % 20 != 0:
-                print "1 nope"
-            else:
-                if y % 19 != 0:
-                    print "2 nope"
-                else:
-                    if y % 18 != 0:
-                        print "3 nope"
-                    else:
-                        if y % 17 != 0:
-                            print "4 nope"
-                        else:
-                            if y % 16 != 0:
-                                print "5 nope"
-                            else:
-                                if y % 15 != 0:
-                                    print "6 nope"
-                                else:
-                                    if y % 14 != 0:
-                                        print "7 nope"
-                                    else:
-                                        if y % 13 != 0:
-                                            print "8 nope"
-                                        else:
-                                            print y
-                                            return
-
-multiple()
+# numbers = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+# 
+# print numbers 
+# 
+# def multiple():
+# 
+#     for y in xrange(6000000, 10000000):
+#             
+#             if y % 20 != 0:
+#                 print "1 nope"
+#             else:
+#                 if y % 19 != 0:
+#                     print "2 nope"
+#                 else:
+#                     if y % 18 != 0:
+#                         print "3 nope"
+#                     else:
+#                         if y % 17 != 0:
+#                             print "4 nope"
+#                         else:
+#                             if y % 16 != 0:
+#                                 print "5 nope"
+#                             else:
+#                                 if y % 15 != 0:
+#                                     print "6 nope"
+#                                 else:
+#                                     if y % 14 != 0:
+#                                         print "7 nope"
+#                                     else:
+#                                         if y % 13 != 0:
+#                                             print "8 nope"
+#                                         else:
+#                                             print y
+#                                             return
+# 
+# multiple()
