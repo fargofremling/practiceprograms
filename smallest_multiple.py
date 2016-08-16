0d33b87730ad63447e14d35973eeb676f7186048
fargofremling <alicia.fremling@gmail.com>
2016-08-15 23:07:35 -0700

    Clean up of  code.

--- smallest_multiple.py
+++ smallest_multiple.py
@@ -14,7 +14,6 @@
 #13
 #12 (2*2*3)
 #11
-#  CONTINUE
 #9 (3*3)
 #8 (2*2*2)
 #7
@@ -24,59 +23,16 @@
 #3
 #2
 
+# largest_factors = 11*12*13*14*15*16*17*18*19*20
+# largest_factors is not correct
+
+# Prime Numbers
 # 1, 2, 3, 5, 7, 11, 13, 17, 19
 
+# prime_product = 2*3*5*7*11*13*17*19
+# prime_product is not correct
+
+# Prime Factors
 number = 2*2*2*2*3*3*5*7*11*13*17*19
 
 print number
-# Prime Numbers
-# 1, 2, 3, 5, 7, 11, 13, 17, 19
-# 
-# prime_product = 2*3*5*7*11*13*17*19
-# 
-# print prime_product
-
-# prime_product is not correct
-
-# largest_factors = 11*12*13*14*15*16*17*18*19*20
-
-# print largest_factors
-
-# largest_factors is not correct
-
-# numbers = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-# 
-# print numbers 
-# 
-# def multiple():
-# 
-#     for y in xrange(6000000, 10000000):
-#             
-#             if y % 20 != 0:
-#                 print "1 nope"
-#             else:
-#                 if y % 19 != 0:
-#                     print "2 nope"
-#                 else:
-#                     if y % 18 != 0:
-#                         print "3 nope"
-#                     else:
-#                         if y % 17 != 0:
-#                             print "4 nope"
-#                         else:
-#                             if y % 16 != 0:
-#                                 print "5 nope"
-#                             else:
-#                                 if y % 15 != 0:
-#                                     print "6 nope"
-#                                 else:
-#                                     if y % 14 != 0:
-#                                         print "7 nope"
-#                                     else:
-#                                         if y % 13 != 0:
-#                                             print "8 nope"
-#                                         else:
-#                                             print y
-#                                             return
-# 
-# multiple()
