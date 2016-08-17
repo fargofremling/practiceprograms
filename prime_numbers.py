5da451b546fa57b03a9871e8f0ee3ea0e2249e15
fargofremling <alicia.fremling@gmail.com>
2016-08-16 21:55:08 -0700

    Finds primes, but not quite right, n is wrong

--- prime_numbers.py
+++ prime_numbers.py
@@ -4,3 +4,13 @@
 # What is the 10,001st prime number?
 
 print "Works!"
+
+n = int(raw_input("What prime number would you like to find?\n> "))
+
+for p in range(2, n+1):
+    for i in range(2, p):
+        if p % i == 0:
+            break
+    else:
+        print p,
+print 'Done'
