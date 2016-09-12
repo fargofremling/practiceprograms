b1741e56a3ef5061aec330fa434e3ec75e1e4509
fargofremling <alicia.fremling@gmail.com>
2016-09-11 22:39:12 -0700

    Working on making PE Problem 10 more efficient.

--- primes_summation.py
+++ primes_summation.py
@@ -3,20 +3,26 @@
 # 
 # Find the sum of all the primes below two million.
 
+import timeit
+
+start = timeit.default_timer()
 
 n = int(raw_input("What prime number would you like to find?\n> "))
 
-primes = [2]
+primes = [2,3]
 
-for p in range(3, n+1, 2):
-
-    for x in range(2, p):
-    
-         if p % x == 0:
-             break
+for p in range(5, n+1, 2):
+    if any(p % i == 0 for i in primes):
+#     p % 3 == 0 or p % 5 == 0 or p % 7 == 0 or p % 11 == 0 or p % 13 == 0:
+        continue
     else:
-        print p
-        primes.append(p)
+        for x in range(2, p):
+    
+             if p % x == 0:
+                 break
+        else:
+            print p
+            primes.append(p)
             
 print primes
          
@@ -26,5 +32,9 @@ def add(x, y):
 sum = reduce(add, primes)
 
 print sum
+
+stop = timeit.default_timer()
+
+print stop - start
 
 # answer: 142913828922
