e2d98d4d123a9fcd950395a61bf3fbc3151714db
fargofremling <alicia.fremling@gmail.com>
2016-09-23 23:54:13 -0700

    Swift playground for practice.

---
+++ LearntheEssentialsofSwift.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var myVariable = 42
+
+myVariable = 50
+
+let myConstant = 42
+
+let implicitInteger = 70
+let implicictDouble = 70.0
+let explicitDouble: Double = 70
+
+let label = "The width is "
+let width = 94
+let widthLabel = label + width
+
