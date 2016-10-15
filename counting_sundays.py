8dc08b722f7a5cb8d204ce10d72d9eb2b625ae92
Alicia <alicia.fremling@gmail.com>
2016-10-14 19:00:42 -0700

    Define year types.

--- counting_sundays.py
+++ counting_sundays.py
@@ -15,7 +15,8 @@
 months = {'jan': 31, 'feb': 28, 'feb_leap': 29, 'mar': 31, 'april': 30, 'may': 31, 'june': 30, 'july': 31, 'aug': 31,
 'sept': 30, 'oct': 31, 'nov': 30, 'dec': 31}
 
-
+# normal year is 365 days
+# leap year is 366 days
 
 def counting_sundays():
     year = 1899
