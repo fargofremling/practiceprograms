03de342a884246301b0be61fe2430185f32c2efb
Alicia <alicia.fremling@gmail.com>
2016-10-15 22:23:24 -0700

    Notes on intended functionality.

--- counting_sundays.py
+++ counting_sundays.py
@@ -11,7 +11,8 @@
 # A leap year occurs on any year evenly divisible by 4, but not on a century unless it is divisible by 400.
 # How many Sundays fell on the first of the month during the twentieth century (1 Jan 1901 to 31 Dec 2000)?
 
-
+# I would like to be able to build this program so that the user can put in any starting date and end date
+# Then they could count how many Sundays (or any day for that matter) that had occurred between those two dates. 
 months = {'jan': 31, 'feb': 28, 'feb_leap': 29, 'mar': 31, 'april': 30, 'may': 31, 'june': 30, 'july': 31, 'aug': 31,
 'sept': 30, 'oct': 31, 'nov': 30, 'dec': 31}
 
