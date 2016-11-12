f5c8b466a722f2fb4e8de412f902844ceedbdbc6
fargofremling <alicia.fremling@gmail.com>
2016-11-11 22:39:54 -0700

    Start of calculating exponent and building reduce function.

--- power_digit_sum.py
+++ power_digit_sum.py
@@ -1,5 +1,15 @@
-# 215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+# 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
 # 
-# What is the sum of the digits of the number 21000?
+# What is the sum of the digits of the number 2^1000?
 
-# True time travel test.
+
+big_number = []
+
+print 2**1000
+
+def add(x,y): 
+    return x+y
+
+answer = reduce(add, big_number)
+
+print answer
