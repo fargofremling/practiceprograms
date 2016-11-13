9e4847288777d89d0ba34ddd3fa57713fb7a4f73
fargofremling <alicia.fremling@gmail.com>
2016-11-12 22:49:32 -0700

    Turn number into string. split string into a list.

--- power_digit_sum.py
+++ power_digit_sum.py
@@ -3,9 +3,13 @@
 # What is the sum of the digits of the number 2^1000?
 
 
-big_number = []
+exponent = 2**1000
 
-print 2**1000
+split = str(exponent)
+
+big_number = list(split)
+
+print big_number
 
 def add(x,y): 
     return x+y
