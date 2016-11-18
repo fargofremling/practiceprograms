8b857adde07a850ad767b208bf72520a2f045c60
Alicia <alicia.fremling@gmail.com>
2016-11-18 00:00:38 -0600

    Start to the algorithm.

--- longest_collatz_sequence.py
+++ longest_collatz_sequence.py
@@ -11,3 +11,9 @@
 # Which starting number, under one million, produces the longest chain?
 # 
 # NOTE: Once the chain starts the terms are allowed to go above one million.
+
+if n % 2 == 0:
+  n = n / 2
+
+if n % 2 != 0:
+   n = 3n + 1
